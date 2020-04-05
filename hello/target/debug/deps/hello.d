53320e70d12028c49a110509d52dfd98914a0bf5
Bruce Zhang <zongtongguilai1@gmail.com>
2020-04-05 22:59:15 +0800

    ee

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\zongtong\Desktop\git\rust_code\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\zongtong\Desktop\git\rust_code\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\zongtong\Desktop\git\rust_code\hello\target\debug\hello.exe: C:\Users\zongtong\Desktop\git\rust_code\hello\src\main.rs
